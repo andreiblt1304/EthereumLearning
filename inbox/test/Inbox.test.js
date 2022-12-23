bc71a902d02661c4de94636b91a31311244b8bbb
Andrei Baltariu <andrei.baltariu@bearingpoint.com>
2022-12-23 23:57:25 +0200

    Added first smart contract test, deploy

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,1 +1,19 @@
-// contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+let accounts;
+
+beforeEach(async() => {
+    accounts = await web3.eth.getAccounts();
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+                    .deploy({ data: bytecode, arguments: ['Initial message'] })
+                    .send({ from: accounts[0], gas: '1000000' })
+});
+
+describe('Inbox testing', () => {
+    it('Deploys a contract', () => {
+        console.log(inbox);
+    });
+});
