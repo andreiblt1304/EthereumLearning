349f9b1dfaad92ccae630f203e050377b9036123
Andrei Baltariu <andrei.baltariu@bearingpoint.com>
2022-12-29 16:21:23 +0200

    Fixed "cannot destruct ABI"

---
+++ Lottery/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+  'imitate cactus same army buyer sword match palace festival stem left crucial',
+  'https://goerli.infura.io/v3/f39e0f16154449f9b36c4efada7a824c'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
